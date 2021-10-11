2fd2400dd109c163a88a01fa7b234d183cd85b43
MosheBiran <73993888+MosheBiran@users.noreply.github.com>
2021-10-11 13:38:41 +0300

    fin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from shutil import copy, move
+from shutil import copy
 from os import listdir, mkdir
 from os.path import isfile, join
 
@@ -8,28 +8,34 @@ assignment_num = ''
 
 
 def folder_reader():
-    # files_names = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+
+    # Creates a submission folder if it does not exist in the system
     try:
         mkdir(destination_path)
     except OSError as error:
-        print(error)
+        pass
 
+    # Goes through all the student submissions files and creates folders according to the required format
     for f in listdir(origin_path):
         if isfile(join(origin_path, f)):
             student_id_path = join(destination_path, f.replace(".ipynb", ''))
 
+            # If the student does not have a submission folder he creates a new folder for him
             try:
                 mkdir(student_id_path)
             except OSError as error:
-                print(error)
+                pass
 
+            # Creates a new path for the current job according to the student's ID
             student_id_path = join(student_id_path, "assignment_" + assignment_num)
 
+            # If the student does not have a folder for the current job it is more for him
             try:
                 mkdir(student_id_path)
             except OSError as error:
-                print(error)
+                pass
 
+            # Copies the student submission file to the newly created folder
             copy(join(origin_path, f), student_id_path)
 
 
